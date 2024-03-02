b82ad64cc2a24823fdf5c60f3e2acb8e817a05d2
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2024-03-02 21:30:56 +0530

    pract 1 added

--- project_hello/main.py
+++ project_hello/main.py
@@ -1,2 +1,12 @@
-def hello():
-  print("Hello")
+def p1():
+  print('''
+import seaborn as sns 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+# Create a dataframe
+df = pd.DataFrame({'x': [10, 38, 5, 7, 300, 11, 113, 165, 17, 19],
+          'y': [28, 4, 6, 8, 10, 122, 14, 16, 158, 200]})
+#Create a joint plot with a kernel density estimate (KDE) and a scatter plot
+sns.jointplot(x='x', y='y', data=df, kind='kde').plot_joint(sns.scatterplot)
+plt.show()
+''')
