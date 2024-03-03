5a03c1c8b5083bc5b32e04ff0fe3fe3e3588f40a
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2024-03-03 20:19:31 +0530

    New Package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name='project_hello',
-    version= '0.4',
+    name='project_hello1',
+    version= '0.5',
     packages=find_packages(),
     install_requires=[]
 )
