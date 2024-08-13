6ac3ce8b1911e9a0d052f702ba83ad86f90c6b67
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2024-08-13 23:27:42 +0530

    changes3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name='project123',
-    version= '2.3',
+    name='project12345',
+    version= '0.0.1',
     packages=find_packages(),
     install_requires=[]
 )
